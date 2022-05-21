774c60f3642d811eac169aac56e8421b2acabd53
Talin Gupta <taling21@iitk.ac.in>
2022-05-21 13:06:37 +0530

    taskA

--- roadsafeA.py
+++ roadsafeA.py
@@ -1,11 +1,10 @@
 import pandas as pd
-
-#from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 
-df = pd.read_csv('houses.csv')
+df = pd.read_csv('kc_house_data.csv')
 df = df.dropna()  #removing rows with empty entries(NaN)
+df = pd.get_dummies(data= df,drop_first=True)  # dealing with string features
 x = df.drop(columns = ['price'])
 y = df['price']
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.1)     #using 90% of dataset for training,rest for testing
